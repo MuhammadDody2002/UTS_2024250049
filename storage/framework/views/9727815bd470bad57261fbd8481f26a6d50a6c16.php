f77a91bf61488b9558a6b3cc26d15abaaa98fdf9
MuhammadDody2002 <muhammaddody05@mhs.mdp.ac.id>
2022-04-11 23:52:14 +0700

    UTS

---
+++ storage/framework/views/9727815bd470bad57261fbd8481f26a6d50a6c16.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel</title>
+</head>
+<body>
+    <h1>Universitas MDP</h1>
+<?php /**PATH D:\Kuliah\Dody\UTS_2024250049\UTS_2024250049\resources\views/layout/header.blade.php ENDPATH**/ ?>
